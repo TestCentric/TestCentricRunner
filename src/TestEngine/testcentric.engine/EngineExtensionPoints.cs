75dcf599f0f97607d2ebf9a9dea48a2af44794d8
Charlie Poole <charliepoole@gmail.com>
2025-09-27 21:28:23 -0700

    Enhanced Extensions Dialog

--- src/TestEngine/testcentric.engine/EngineExtensionPoints.cs
+++ src/TestEngine/testcentric.engine/EngineExtensionPoints.cs
@@ -6,8 +6,13 @@ using TestCentric.Engine.Extensibility;
 using TestCentric.Extensibility;
 
 // ExtensionPoints supported by the engine
-[assembly: ExtensionPoint("/TestCentric/Engine/AgentLaunchers", typeof(NUnit.Engine.Extensibility.IAgentLauncher))]
-[assembly: ExtensionPoint("/TestCentric/Engine/ProjectLoaders", typeof(NUnit.Engine.Extensibility.IProjectLoader))]
-[assembly: ExtensionPoint("/TestCentric/Engine/ResultWriters", typeof(NUnit.Engine.Extensibility.IResultWriter))]
-[assembly: ExtensionPoint("/TestCentric/Engine/TestEventListeners", typeof(NUnit.Engine.ITestEventListener))]
-[assembly: ExtensionPoint("/TestCentric/Engine/DriverFactories", typeof(NUnit.Engine.Extensibility.IDriverFactory))]
+[assembly: ExtensionPoint("/TestCentric/Engine/AgentLaunchers", typeof(NUnit.Engine.Extensibility.IAgentLauncher),
+    Description = "Launches an Agent Process for supported target runtimes")]
+[assembly: ExtensionPoint("/TestCentric/Engine/ProjectLoaders", typeof(NUnit.Engine.Extensibility.IProjectLoader),
+    Description = "Recognizes and loads assemblies from various types of project formats.")]
+[assembly: ExtensionPoint("/TestCentric/Engine/ResultWriters", typeof(NUnit.Engine.Extensibility.IResultWriter),
+    Description = "Supplies a writer to write the result of a test to a file using a specific format.")]
+[assembly: ExtensionPoint("/TestCentric/Engine/TestEventListeners", typeof(NUnit.Engine.ITestEventListener),
+    Description = "Allows an extension to process progress reports and other events from the test.")]
+[assembly: ExtensionPoint("/TestCentric/Engine/DriverFactories", typeof(NUnit.Engine.Extensibility.IDriverFactory),
+    Description = "Supplies a driver to run tests that use a specific test framework.")]
