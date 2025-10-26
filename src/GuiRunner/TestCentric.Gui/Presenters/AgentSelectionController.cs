330dcce7feacd1a41a21ad8aeb466da300cade9b
rowo360 <59574371+rowo360@users.noreply.github.com>
2025-10-26 16:56:45 +0100

    Agent selection menu - register click event only once

--- src/GuiRunner/TestCentric.Gui/Presenters/AgentSelectionController.cs
+++ src/GuiRunner/TestCentric.Gui/Presenters/AgentSelectionController.cs
@@ -3,6 +3,7 @@
 // Licensed under the MIT License. See LICENSE file in root directory.
 // ***********************************************************************
 
+using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
 using TestCentric.Engine;
@@ -40,6 +41,7 @@ namespace TestCentric.Gui.Presenters
             defaultItem.Checked = true;
             defaultItem.Enabled = true; // Should always remain enabled
             agentMenu.MenuItems.Add(defaultItem);
+            defaultItem.Click += OnAgentMenuItemClicked;
 
             foreach (var agentFullName in _model.AvailableAgents)
             {
@@ -52,6 +54,7 @@ namespace TestCentric.Gui.Presenters
                 var agentItem = new ToolStripMenuItem(agentName);
                 agentItem.Tag = agentFullName;
                 agentMenu.MenuItems.Add(agentItem);
+                agentItem.Click += OnAgentMenuItemClicked;
             }
         }
 
@@ -68,39 +71,40 @@ namespace TestCentric.Gui.Presenters
                 {
                     string itemTag = item.Tag as string;
                     item.Enabled = itemTag == "DEFAULT" || agentsToEnable.Contains(itemTag);
-
-                    item.Click += (s, e) =>
-                    {
-                        if (!item.Checked)
-                        {
-                            EnsureSingleItemChecked(item);
-                            item.Enabled = agentsToEnable.Contains(itemTag);
-
-                            // TODO: We currently need to use both settings. Investigate.
-                            if (itemTag == null || itemTag == "DEFAULT")
-                            {
-                                _model.TestCentricProject.RemoveSetting(SettingDefinitions.SelectedAgentName);
-                                _model.TestCentricProject.RemoveSetting(SettingDefinitions.RequestedAgentName);
-                            }
-                            else
-                            {
-                                _model.TestCentricProject.AddSetting(SettingDefinitions.SelectedAgentName.WithValue(itemTag));
-                                _model.TestCentricProject.AddSetting(SettingDefinitions.RequestedAgentName.WithValue(itemTag));
-                            }
-
-                            // Even though the _model has a Reload method, we cannot use it because Reload
-                            // does not re-create the Engine.  Since we just changed a setting, we must
-                            // re-create the Engine by unloading/reloading the tests. We make a copy of
-                            // __model.TestFiles because the method does an unload before it loads.
-                            _model.TestCentricProject.LoadTests();
-                        }
-                    };
                 }
+            }
+        }
+
+        private void OnAgentMenuItemClicked(object sender, EventArgs e)
+        {
+            ToolStripMenuItem item = sender as ToolStripMenuItem;
+            if (!item.Checked)
+            {
+                EnsureSingleItemChecked(item);
+
+                // TODO: We currently need to use both settings. Investigate.
+                string itemTag = item.Tag as string;
+                if (itemTag == null || itemTag == "DEFAULT")
+                {
+                    _model.TestCentricProject.RemoveSetting(SettingDefinitions.SelectedAgentName);
+                    _model.TestCentricProject.RemoveSetting(SettingDefinitions.RequestedAgentName);
+                }
+                else
+                {
+                    _model.TestCentricProject.AddSetting(SettingDefinitions.SelectedAgentName.WithValue(itemTag));
+                    _model.TestCentricProject.AddSetting(SettingDefinitions.RequestedAgentName.WithValue(itemTag));
+                }
+
+                // Even though the _model has a Reload method, we cannot use it because Reload
+                // does not re-create the Engine.  Since we just changed a setting, we must
+                // re-create the Engine by unloading/reloading the tests. We make a copy of
+                // __model.TestFiles because the method does an unload before it loads.
+                _model.TestCentricProject.LoadTests();
             }
 
             void EnsureSingleItemChecked(ToolStripMenuItem itemToCheck)
             {
-                foreach (ToolStripMenuItem item in agentMenu.MenuItems)
+                foreach (ToolStripMenuItem item in _view.SelectAgentMenu.MenuItems)
                     item.Checked = false;
                 itemToCheck.Checked = true;
             }
