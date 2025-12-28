a7654b249b60c3674c9604d92c39b76fd678f31b
Wolff, Roald (DI FA HMI EE PRC3) <roald.wolff@siemens.com>
2025-12-28 10:18:33 +0100

    Add missing file heder

--- src/GuiRunner/TestCentric.Gui.Tests/Presenters/AgentSelectionControllerTests.cs
+++ src/GuiRunner/TestCentric.Gui.Tests/Presenters/AgentSelectionControllerTests.cs
@@ -1,4 +1,7 @@
-
+// ***********************************************************************
+// Copyright (c) Charlie Poole and TestCentric contributors.
+// Licensed under the MIT License. See LICENSE file in root directory.
+// ***********************************************************************
 
 namespace TestCentric.Gui.Presenters
 {
